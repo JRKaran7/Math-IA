0dbc54e75297771627994f064e27ca82e01a983f
JRKaran7 <137197958+JRKaran7@users.noreply.github.com>
2024-11-08 23:14:48 +0530

    math_ia.py

--- math_ia.py
+++ math_ia.py
@@ -2,8 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import sklearn
+import sklearn.preprocessing
+from scipy.stats import skew, kurtosis
 
-dataset = pd.read_csv('netflix_titles.csv')
+dataset = pd.read_csv('netflix_titles.csv', encoding='ISO-8859-1')
 print(dataset.head())
 print(dataset.info())
 
@@ -22,9 +25,42 @@ print(f'Number of duplicated values: {dataset.duplicated().sum()}')
 dataset['date_added'] = dataset['date_added'].apply(pd.to_datetime)
 print(f'Data type of date_added: {dataset["date_added"].dtype}')
 
+df_corr = dataset[['release_year', 'duration']].dropna()
+print("\nSkewness and Kurtosis:")
+for column in ['release_year', 'duration_mins']:
+    if column in df_corr.columns:
+        print(f"{column}: Skewness={skew(df_corr[column]):.2f}, Kurtosis={kurtosis(df_corr[column]):.2f}")
 plt.figure(figsize=(6, 4))
-sns.countplot(data=dataset, x='type', palette='coolwarm')
+c1 = dataset['type'].value_counts()
+
+print(c1)
+c1.plot(kind='bar')
+plt.xlabel('Type')
+plt.ylabel('Frequency')
 plt.title('Count of Movies vs TV Shows')
+plt.show()
+
+def remove_outlier(col):
+    sorted(col)
+    Q1,Q3=np.percentile(col,[25,75])
+    IQR=Q3-Q1
+    lower_range= Q1-(1.5 * IQR)
+    upper_range= Q3+(1.5 * IQR)
+    return lower_range, upper_range
+
+le = sklearn.preprocessing.LabelEncoder()
+dataset['rating'] = le.fit_transform(dataset['rating'])
+
+lr,ur=remove_outlier(dataset['rating'])
+dataset['rating']=np.where(dataset['rating']>ur,ur,dataset['rating'])
+dataset['rating']=np.where(dataset['rating']<lr,lr,dataset['rating'])
+
+plt.figure(figsize=(7.5, 4.5))
+sns.boxplot(dataset['rating'],whis=2)
+plt.title('Box and Whisker Plot of Ratings')
+plt.show()
+sns.violinplot(dataset['rating'])
+plt.title('Violin Plot')
 plt.show()
 
 dataset['duration'] = dataset['duration'].apply(lambda x: int(str(x).replace(' min', '')) if 'min' in str(x) else 0)
